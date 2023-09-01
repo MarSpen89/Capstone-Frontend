7402e44d576b92f11289f88acbc03b43e1dda662
Marcedes <118416606+MarSpen89@users.noreply.github.com>
2023-08-31 22:47:47 -0400

    styling

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,70 +1,94 @@
 // This page will render a Log in button if your not logged in, upon clicking will lead you to a login page.
 // If you're logged in, it will render a Log out button with your email address.
 
-import React from "react"
-// import { useNavigate } from "react-router-dom";
-// import Login from "../../components/auth/login";
-// import "..//..//components/"
-import styled from 'styled-components';
+import React, { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/home.scss";
 
-const StyledButton = styled.button`
-    padding: 12px 24px;
-    margin: 8px;
-    background-color: cornflowerblue;
-    color: white;
-    border: none;
-    border-radius: 8px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 40px;
-`;
+const Home = ({ loggedIn, email }) => {
+  const navigate = useNavigate();
+  const canvasRef = useRef(null);
 
-const StyledInput = styled.input`
-    padding: 10px;
-    margin: 10px;
-    width: 80%;
-    transition: background-color 0.7s ease;
-`;
+  const onButtonClick = () => {
+    // Update this function later
+  };
 
-const StyledDiv = styled.div`
-    padding: 12px 24px;
-    margin: 8px;
-    font-style: 40px
-`;
+  const resizeCanvas = () => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  };
 
-function App() {
-    return <StyledButton>Click Me</StyledButton>;
-}
+  const drawFlames = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    const particles = [];
 
-const Home = (props) => {   // passing data from one api to another
-    const { loggedIn, email } = props
-    // const navigate = useNavigate();
-    
-    const onButtonClick = () => {
-        // I'll update this function later
-    };
-
-    
-        return(
-            <StyledDiv>
-            <div className="mainContainer">
-            <div className={"titleContainer"}>
-                <div>Welcom!</div>
-            </div>
-            This is the home page
-
-            <StyledButton type="submit">Login</StyledButton>
-            <div>
-
-            </div>
-            </div>
-            </StyledDiv>
-        );
+    for (let i = 0; i < 5; i++) {
+      particles.push({
+        x: Math.random() * canvas.width,
+        y: canvas.height,
+        speed: Math.random() * 5 + 1,
+        size: Math.random() * 5 + 5,
+        life: Math.random() * 30 + 50,
+      });
     }
 
+    for (const p of particles) {
+      ctx.fillStyle = `rgba(0, 0, 255, ${p.life / 100})`;
+      ctx.beginPath();
+      ctx.arc(p.x, p.y, p.size, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].y -= particles[i].speed;
+      particles[i].life--;
+
+      if (particles[i].life <= 0) {
+        particles.splice(i, 1);
+        i--;
+      }
+    }
+
+    requestAnimationFrame(drawFlames);
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    resizeCanvas();
+    drawFlames();
+
+    window.addEventListener("resize", resizeCanvas);
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+    };
+  }, []);
+
+  return (
+    <div className="mainContainer">
+      <canvas ref={canvasRef} id="flameCanvas"></canvas>
+      <div className="titleContainer">
+        <div>Welcome Back!</div>
+      </div>
+      <div>This is the home page.</div>
+      <div className="buttonContainer">
+        <input
+          className="inputButton"
+          type="button"
+          onClick={onButtonClick}
+          value={loggedIn ? "Log out" : "Log in"}
+        />
+        {loggedIn && <div>Your email address is {email}</div>}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
