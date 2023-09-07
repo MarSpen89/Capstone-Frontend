4ee23be979b1c94b7fe1743567b5c117add65a61
Marcedes <118416606+MarSpen89@users.noreply.github.com>
2023-09-06 23:45:41 -0400

    input box

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,21 +1,25 @@
 // This page will render a Log in button if your not logged in, upon clicking will lead you to a login page.
 // If you're logged in, it will render a Log out button with your email address.
 
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // Removed unused import: import Login from "../../components/auth/login";
 import "../styles/home.scss";
 
-
-
 const Home = (props) => { // TODO: Describe how data is passed from one API to another
     const { loggedIn, email } = props;
     const navigate = useNavigate();
-    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const onButtonClick = () => {
-        navigate('/profile') 
-        console.log("Button Clicked!")
+        if (loggedIn) {
+            // Handle logout logic here (e.g, call API to log the user out)
+            console.log("Log out clicked");
+        } else {
+            // Handle login logic here (e.g., navigate to login page)
+            navigate('/login');
+        }
     };
 
     return (
@@ -23,6 +27,7 @@ const Home = (props) => { // TODO: Describe how data is passed from one API to a
             <div className="titleContainer">
                 <div>Welcome Back!</div>
             </div>
+
             <div>
                 This is the home page.
                 {loggedIn && (
@@ -32,17 +37,25 @@ const Home = (props) => { // TODO: Describe how data is passed from one API to a
                     </div>
                 )}
             </div>
+
             <div className="buttonContainer">
-                <input
-                    className="inputButton"
-                    type="button"
-                    onClick={onButtonClick}
-                    value={loggedIn ? "Log out" : "Log in"} 
-                />
-                {loggedIn ? <div>Your email address is {email}</div> : null}
+                {!loggedIn ? (
+                    <div>
+                        {/* Username Input box */}
+                        <input
+                            className="inputButton"
+                            type="button"
+                            onClick={onButtonClick}
+                            value={loggedIn ? "Log out" : "Log in"}
+                        />
+                    </div>
+                ) : (
+                    <div>Your email address is {email}</div>
+                )}
             </div>
         </div>
     );
 };
+
 
 export default Home;
